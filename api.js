c8c559e3a99aef3df5e618e80e385740d719e231
Dan <danefes@gmail.com>
2019-02-27 09:43:36 +0200

    make it better

--- api.js
+++
@@ -1,5 +0,0 @@
-var router = require('express').Router();
-router.use('/products', require('./Product/ProductsRoutes'));
-router.use('/posts', require('./Post/PostRoutes'));
-
-module.exports = router;
