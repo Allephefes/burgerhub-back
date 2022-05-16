e315825be30ce25ee31b60adae076852f188e486
unknown <danefes@gmail.com>
2022-05-16 13:13:08 +0300

    initial commit

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', api);
 
-let strConnect = "mongodb://asaf:asaf@cluster0-shard-00-00-dn6sy.mongodb.net:27017,cluster0-shard-00-01-dn6sy.mongodb.net:27017,cluster0-shard-00-02-dn6sy.mongodb.net:27017/product?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 const OPT = { useNewUrlParser: true };
-mongoose.connect(strConnect, OPT);
 let port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log("Running om port " + port);
