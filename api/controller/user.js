6e61ffb7bc149a77d5ae179a3491ad74fe9bb53b
unknown <danefes@gmail.com>
2022-05-20 12:58:25 +0300

    added more user functions

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,11 +1,23 @@
 let {user} = require('../services');
 
 exports.create = async function (req, res, next) {
-    let [id] = await user.createUser(req.body.username, req.body.password, req.body.auth);
+    let body = req.body;
+    let [id] = await user.createUser(body.username, body.password, body.auth);
     res.json(id);
 };
 
 exports.get = async function (req, res, next) {
     let body = await user.getUser(req.body.id);
     res.json(body);
+};
+
+exports.update = async function (req, res, next) {
+    let body = req.body;
+    await user.updateUser(body.id, body.username, body.password, body.auth);
+    res.json(body);
+};
+
+exports.delete = async function (req, res, next) {
+    let [id] = await user.deleteUser(req.body.id);
+    res.json(id);
 };
--- api/services/user.js
+++ api/services/user.js
@@ -14,6 +14,22 @@ class User {
             id:id
         }).select('*');
     }
+
+    async updateUser(id, username, password, auth) {
+        return await db('users').where({
+            id:id
+        }).update({
+            username:username,
+            password:password,
+            auth:auth
+        });
+    }
+
+    async deleteUser(id, username, password, auth) {
+        return await db('users').where({
+            id:id
+        }).del().returning('id');
+    }
 }
 
 module.exports = new User();
